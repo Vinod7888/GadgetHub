867157f2f41b704d24935e5feff9f11f9978d585
vinod prajapat <vinodprajapat7888@gmail.com>
2025-07-24 11:29:19 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// var btn1 = document.querySelector("#btn1");
+// var btn2 = document.querySelector("#btn2");
+// var box = document.querySelectorAll(".box");
+
+// var count = 0;
+// btn1.addEventListener(
+//     "click",
+//     function () {
+//         if (count < box.length - 1) {
+//             count++;
+//             for (var data of box) {
+//                 data.style.transform = `translateX(-${count * 100}%)`;
+//             }
+//         }
+//         else {
+//             // if (count == box.length) {
+//             //     count = 0;
+//             //     for (var data of box) {
+//             //         data.style.transform = `translateX(-${count * 100}%)`;
+//             //     }
+//             // }
+//         }
+//     }
+// )
+// btn2.addEventListener(
+//     "click",
+//     function () {
+//         if (count >= 1) {
+//             count--;
+//             for (var data of box) {
+//                 data.style.transform = `translateX(-${count * 100}%)`;
+//             }
+//         }
+
+//     }
+// )
